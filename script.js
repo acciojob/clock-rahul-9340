60fcc38007f3652427555740ef809cfd84db2249
rahul-9340 <142489789+rahul-9340@users.noreply.github.com>
2023-10-07 11:47:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+
+
+        function updateClock() {
+            const ptag = document.getElementById("timer");
+            const date = new Date();
+            ptag.innerText = date.toString();
+        }
+
+        // Update the clock every second (1000 milliseconds)
+        setInterval(updateClock, 1000);
+
+        // Initial update to show the current time immediately
+        updateClock();
+    
+
+
+
+
+
